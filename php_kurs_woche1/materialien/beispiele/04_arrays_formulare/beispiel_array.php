dc28fcfd7ee561069a17ca348ac4d26c8c1312c7
nrain116 <nrain116@protonmail.com>
2025-11-13 11:42:30 +0000

    Array-Beispiele hinzugefügt

--- php_kurs_woche1/materialien/beispiele/04_arrays_formulare/beispiel_array.php
+++ php_kurs_woche1/materialien/beispiele/04_arrays_formulare/beispiel_array.php
@@ -1,18 +1,122 @@
 <?php
-declare(strict_types=1);
 
-?><!doctype html>
+declare(strict_types=1);
+error_reporting(E_ALL);
+ini_set('display_errors', true);
+
+/* indizierte Arrays */
+$posts = ['Post 1', 'Post 2', 'Post 3'];
+$cities = array(
+  'Leipzig',
+  'Nordhausen',
+  'Erfurt'
+);
+
+/* assoziative Arrays */
+$capitalCities = array(
+  'Schweiz' => 'Bern',
+  'Frankreich' => 'Paris',
+  'Deutschland' => 'Berlin'
+);
+
+/* mehrdimensionale Arrays */
+$posts2 = array(
+  array(
+    'title' => 'Erster Beitrag',
+    'author' => 'Alex',
+    'content' => 'Willkommen im Blog!'
+  ),
+  array(
+    'title' => 'Zweiter Beitrag',
+    'author' => 'Sam',
+    'content' => 'Heute lernen wir Arrays.'
+  )
+
+);
+
+/* Arrays für tabellarische Ausgaben */
+$laender = array(
+  'Spanien' => array(
+    'Hauptstadt' => 'Madrid',
+    'Sprache' => 'spanisch',
+    'Waehrung' => 'Euro',
+    'Flaeche' => 504645
+  ),
+  'England' => array(
+    'Hauptstadt' => 'London',
+    'Sprache' => 'englisch',
+    'Waehrung' => 'Pfund Sterlin',
+    'Flaeche' => 130321
+  ),
+  'Portuugal' => array(
+    'Hauptstadt' => 'Lissabon',
+    'Sprache' => 'portugesisch',
+    'Waehrung' => 'Euro',
+    'Flaeche' => 130131
+  ),
+)
+?>
+<!doctype html>
 <html lang="de">
+
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width,initial-scale=1">
   <title>Arrays Beispiel</title>
   <link rel="stylesheet" href="../../style/style.css">
 </head>
+
 <body>
-  <header><h1>Beiträge aus Arrays</h1></header>
+  <header>
+    <h1>Beiträge aus Arrays</h1>
+  </header>
   <main class="container">
-    
+    <?php foreach ($posts as $post): ?>
+      <p><?= $post ?></p>
+    <?php endforeach; ?>
+    <p>Zweite Stadt im Array der Städte: <?= $cities[1] ?></p>
+
+    <?php $country = 'Schweiz'; ?>
+    <p>Die Hauptstadt von <?= $country ?> ist <?= $capitalCities[$country] ?></p>
+
+    <?php foreach ($capitalCities as $country => $city): ?>
+      <p><?= $country ?> : <?= $city ?></p>
+    <?php endforeach; ?>
+
+    <h2>Unsere aktuellen Beiträgen</h2>
+
+    <?php foreach ($posts2 as $p): ?>
+      <article class="post">
+        <h3><?= htmlspecialchars($p['title']) ?></h3>
+        <p class="meta">von <?= htmlspecialchars($p['author']) ?></p>
+        <p><?= nl2br(htmlspecialchars($p['content'])) ?></p>
+      </article>
+    <?php endforeach; ?>
+
+    <h2>Informationen zu Ländern</h2>
+    <table>
+      <tr>
+        <th>Land</th>
+        <th>Hauptstadt</th>
+        <th>Sprache</th>
+        <th>Währung</th>
+        <th>Fläche</th>
+      </tr>
+
+      <?php foreach ($laender as $land => $infos): ?>
+        <!-- äußere Schleife für die Zeilen und das Land -->
+        <tr>
+          <td><?= $land ?></td>
+
+          <!-- innere Schleife für die Info-Zellen -->
+          <?php foreach ($infos as $info): ?>
+            <td><?= $info ?></td>
+          <?php endforeach; ?>
+        </tr>
+      <?php endforeach; ?>
+
+    </table>
   </main>
 </body>
+
 </html>
--- php_kurs_woche1/materialien/beispiele/04_arrays_formulare/beispiel_formular.php
+++ php_kurs_woche1/materialien/beispiele/04_arrays_formulare/beispiel_formular.php
@@ -1,19 +1,27 @@
 <?php
+
 declare(strict_types=1);
+error_reporting(E_ALL);
+ini_set('display_errors', true);
 
 ?>
 <!doctype html>
 <html lang="de">
+
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width,initial-scale=1">
   <title>Formular Beispiel</title>
   <link rel="stylesheet" href="../../style/style.css">
 </head>
+
 <body>
-  <header><h1>Formular & Validierung</h1></header>
+  <header>
+    <h1>Formular & Validierung</h1>
+  </header>
   <main class="container">
-    
+
   </main>
 </body>
+
 </html>
