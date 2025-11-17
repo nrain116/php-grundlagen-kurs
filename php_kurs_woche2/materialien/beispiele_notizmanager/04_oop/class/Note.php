77adf36e12fe936a75b465c4096f8411f17ba4d5
nrain116 <nrain116@protonmail.com>
2025-11-17 13:35:54 +0000

    Beispiel; erste Objekte anlegen

--- php_kurs_woche2/materialien/beispiele_notizmanager/04_oop/class/Note.php
+++ php_kurs_woche2/materialien/beispiele_notizmanager/04_oop/class/Note.php
@@ -1,3 +1,30 @@
 <?php
+
 declare(strict_types=1);
 
+class Note
+{/*  Öffentlich
+    public string $title;
+    public string $content;
+
+    public function __construct(string $title, string $content)
+    {
+        $this->title = $title;
+        $this->content = $content;
+    } */
+
+    public function __construct(private string $title, private string $content)
+    {
+        //
+    }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function getContent()
+    {
+        return $this->content;
+    }
+}
--- php_kurs_woche2/materialien/beispiele_notizmanager/04_oop/index.php
+++ php_kurs_woche2/materialien/beispiele_notizmanager/04_oop/index.php
@@ -1,18 +1,37 @@
 <?php
-declare(strict_types=1);
 
-?><!doctype html>
+declare(strict_types=1);
+error_reporting(E_ALL);
+ini_set('display_errors', true);
+require_once __DIR__ . '/class/Note.php';
+$notes = [
+  new Note('Erster Eintrag', 'OOP macht PHP strukturierter.'),
+  new Note('Zweiter Eintrag', 'Klassen kapseln Daten & Verhalten.')
+]
+
+?>
+<!doctype html>
 <html lang="de">
+
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width,initial-scale=1">
   <title>OOP Beispiel</title>
   <link rel="stylesheet" href="../../style/style.css">
 </head>
+
 <body>
-<header><h1>OOP – Klasse Note</h1></header>
-<main class="container">
-  
-</main>
+  <header>
+    <h1>OOP – Klasse Note</h1>
+  </header>
+  <main class="container">
+    <?php foreach ($notes as $n): ?>
+      <article class="post">
+        <h2><?= htmlspecialchars($n->getTitle()) ?></h2>
+        <p><?= htmlspecialchars($n->getContent()) ?></p>
+      </article>
+    <?php endforeach; ?>
+  </main>
 </body>
+
 </html>
